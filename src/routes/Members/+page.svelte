720ac1262c35194e229f553d8d20a474f542f310
Yamada Keigoe <keigo2002456@icloud.com>
2025-04-21 14:43:39 +0900

    member追加

--- src/routes/Members/+page.svelte
+++ src/routes/Members/+page.svelte
@@ -133,13 +133,13 @@
 	
 			<!-- 加藤 -->
 			<div class="flex flex-col md:flex-row gap-6 items-start">
-				<img src="/members/b4_suzuki.jpg" alt="B4写真" class="w-40 h-40 object-cover rounded-lg shadow-md" />
+				<img src="/KatoKeisuke.png" alt="B4写真" class="w-40 h-40 object-cover rounded-lg shadow-md" />
 				<div class="space-y-2 max-w-3xl">
 					<h3 class="text-xl font-semibold text-gray-800">加藤 圭介（Kato Keisuke）</h3>
 					<div class="h-2"></div>
-					<p class="text-gray-700 text-base leading-relaxed">〇〇〇〇〇〇〇〇〇〇〇〇〇〇。</p>
+					<p class="text-gray-700 text-base leading-relaxed">遠回りをして帰ることが好きです。</p>
 					<div class="h-2"></div>
-					<p class="text-gray-600 text-base">研究テーマ：〇〇〇〇〇〇〇〇〇〇〇〇〇〇</p>
+					<p class="text-gray-600 text-base">研究テーマ：画像比較を用いた象嵌工芸品の成分分析(予定)</p>
 					<div class="h-5"></div>
 				</div>
 			</div>
@@ -150,9 +150,9 @@
 				<div class="space-y-2 max-w-3xl">
 					<h3 class="text-xl font-semibold text-gray-800">富松 大輝（Tomimatsu Haruki）</h3>
 					<div class="h-2"></div>
-					<p class="text-gray-700 text-base leading-relaxed">〇〇〇〇〇〇〇〇〇〇〇〇〇〇。</p>
+					<p class="text-gray-700 text-base leading-relaxed">アメフト部で主将しています.声出しと力仕事は任せてください。</p>
 					<div class="h-2"></div>
-					<p class="text-gray-600 text-base">研究テーマ：〇〇〇〇〇〇〇〇〇〇〇〇〇〇</p>
+					<p class="text-gray-600 text-base">研究テーマ：検討中</p>
 					<div class="h-5"></div>
 				</div>
 			</div>
